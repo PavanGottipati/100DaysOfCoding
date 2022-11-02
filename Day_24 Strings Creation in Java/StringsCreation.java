453f101c0d173657f0ded5ee78bc6b9d70829b7f
Gottipati Venkata Naga Pavan <114672441+PavanGottipati@users.noreply.github.com>
2022-11-02 14:56:39 +0530

    Create StringsCreation.java

---
+++ Day_24 Strings Creation in Java/StringsCreation.java
@@ -0,0 +1,12 @@
+//Java Program to create String 
+public class StringsCreation {
+ public static void main(String [] args)
+ {
+    String str="Rakesh";
+    System.out.println("The String created as an String literal is ");
+    System.out.println(str);
+    String st=new String("Kumar");
+    System.out.println("The String created through new keyword i.e., Object is ");
+    System.out.println(st);
+ }   
+}
