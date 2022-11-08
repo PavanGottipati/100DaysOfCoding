c80b846b03dd01fa94a2dae3d33c9fdadee132c5
Gottipati Venkata Naga Pavan <114672441+PavanGottipati@users.noreply.github.com>
2022-11-08 16:08:44 +0530

    Create VowelSuccession.java

---
+++ Day_30 Count Vowel Succession in String/VowelSuccession.java
@@ -0,0 +1,30 @@
+//Java program to find the occurances of two vowels in succession.
+import java.util.Scanner;
+class VowelSuccession
+{
+  public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a line ");
+        String str=sc.nextLine();
+        String strArr[]=str.split(" ");
+        int count=0;
+        char ch1,ch2;
+        for(String s:strArr)
+        {
+            for(int i=0;i<s.length()-1;i++)
+            {
+                ch1=s.charAt(i);
+                ch2=s.charAt(i+1);
+                String p="";
+                p=p+ch1+ch2;
+                if((p.equals("ae")) || (p.equals("ai")) || (p.equals("ao")) || (p.equals("au")) || (p.equals("ea")) || (p.equals("ei")) || (p.equals("eo")) || (p.equals("eu")) || (p.equals("ia")) || (p.equals("ie")) || (p.equals("io")) || (p.equals("iu")) || (p.equals("oa")) || (p.equals("oe")) || (p.equals("oi")) || (p.equals("ou")) || (p.equals("ua")) || (p.equals("ue")) || (p.equals("ui")) || (p.equals("uo")))
+                {
+                    count++;
+                    System.out.println(p);
+                }
+            }
+        }
+        System.out.println("The no of vowel successions are "+count);
+    }
+}
