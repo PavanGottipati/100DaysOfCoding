840b799a6d753a4afbf00706cbd657cf22480a19
Gottipati Venkata Naga Pavan <114672441+PavanGottipati@users.noreply.github.com>
2023-01-20 14:34:07 +0530

    Create App.java

---
+++ Day_95 Applet Programming/App.java
@@ -0,0 +1,10 @@
+//Java program to implement Applets
+import java.awt.Graphics;
+import java.applet.*;
+public class App extends Applet
+{
+  public void paint(Graphics g)
+  {
+    g.drawString("Hello Java",10,10);
+  }
+}
